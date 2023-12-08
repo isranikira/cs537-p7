c8bf50fe84e8a1f2cf2a74a23db7f04baf99c840
isranikira <146687208+isranikira@users.noreply.github.com>
2023-12-08 12:21:52 -0600

    1 TEST BABY!!!

---
+++ fsck.wfs.c
@@ -0,0 +1,5 @@
+//this file neeeds to exist to compile
+int main(int argc, char *argv[])
+{
+    return 0;
+}
--- mount.wfs.c
+++ mount.wfs.c
@@ -11,19 +11,21 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <errno.h>
 #include <stdlib.h>
 #include "wfs.h"
 
-static const char *logfs_path = "/";
 uint32_t start_of_logs;
-void *curr_mmap_loc;
+static char *mapped_mem = NULL;
 
 // Filesystem methods
 
 //have a global var for the size of the superblock
 //have a global void pointer for where you are currently in the mmap
 //attach the mmap very similar to how you do it in the mkfs file 
+
 //once you have that add the superblock size to the void pointer
+//need to parse the path from '/' to find  the next
 //then able to cast the pointer to a log entry and find if the path is equal to path you are looking for
 //if it is keep track of it and continue else just contiue
 //increament by tge log entry size as well as the sive of its data
@@ -32,41 +34,100 @@ void *curr_mmap_loc;
 //for finding the next file/dir in the path start at the  beginning as a dir and its file do not need to be sequential in the mmap
 struct wfs_log_entry *get_log_entry(const char *path)
 {
-    //void *
-    //TODO
+    /* TODO: change the root logic so that it check if path equals exactly '/'
+    */
+    //can check if this is assigned to anything
+    struct wfs_log_entry *entry_match = NULL;
+    //curr_mmap_loc = start_of_logs; //TODO: change this assigned a int to a void pointer currently
+    unsigned int curr_inode = 0; //start at the root node
+    //set up strtok 
+    char temp_path[MAX_FILE_NAME_LEN];
+    strncpy(temp_path, path, MAX_FILE_NAME_LEN);
+    temp_path[MAX_FILE_NAME_LEN-1] = '\0';
+    int found = 0;
+    //int new_inode_found = 0; //if iterate the first nested while loop and dont find ne
+    
+    char *token = strtok(temp_path, "/");
+    //while the token is not null there is something left in the path
+    //always starts with inode 0 comp token to find the next indoe to look for
+    do
+    {
+        char *curr_mmap_loc = mapped_mem + sizeof(struct wfs_sb);
+        found = 0;
+        //iterate through the entire list to find a location of the most recent entry of what you are looking for
+        while(curr_mmap_loc < mapped_mem + ((struct wfs_sb *)mapped_mem)->head)
+        {
+            struct wfs_inode inode_comp = ((struct wfs_log_entry*)curr_mmap_loc)->inode;
+            if(inode_comp.inode_number ==  curr_inode && inode_comp.deleted == 0)
+            {
+                entry_match = (struct wfs_log_entry *) curr_mmap_loc;
+                //new_inode_found = 1;
+            }
+            curr_mmap_loc = curr_mmap_loc + sizeof(struct wfs_inode) + inode_comp.size;
+        }
+        //first run and might need to exit if only root node 
+        //so that you are not comparing  a null token
+        if(token == NULL)
+        {
+            return entry_match;
+        }
+        
+        //found a place where the log entry wass
+        //now need to find next inode to look for or return the log entry if its the last one
+        //do this by starting at the location of data and parsing through how many entries there are
+        struct wfs_dentry *curr_dentry = (struct wfs_dentry *)entry_match->data;
+        int data_offset = 0;
+        while(data_offset < entry_match->inode.size)
+        {
+            if(strcmp(curr_dentry->name, token) == 0)
+            {
+                found = 1;
+                curr_inode = curr_dentry->inode_number;
+                break; //data struct youre currently on matches the name therfore you have the new inode num to look for
+            }
+            data_offset = data_offset + sizeof(struct wfs_dentry);
+            curr_dentry = curr_dentry + 1;
+        } 
+        //might need to check that somethings was found before going to next token
+        token = strtok(NULL, "/");
+    }while (token != NULL);
+
+    if(found == 0)
+    {
+        return NULL;
+    }  
+    return entry_match;
+
 }
 
 
 static int wfs_getattr(const char *path, struct stat *stbuf)
 {
-    memset(stbuf, 0, sizeof(struct stat));
-
-    if (strcmp(path, "/") == 0) 
+    struct wfs_log_entry *curr_log_entry = get_log_entry(path);
+    if(curr_log_entry == NULL)
     {
-        stbuf->st_mode = __S_IFDIR;
-        stbuf->st_nlink = 2;
-        return 0;
+        return -ENOENT; //path was not found
     }
-    stbuf->st_mode = __S_IFREG;
-    stbuf->st_nlink = 1;
-    stbuf->st_uid = getuid();
-	stbuf->st_gid = getgid();
-	stbuf->st_mtime = time( NULL );
-    stbuf->st_size = sizeof(struct wfs_log_entry); //might need to change this later
+    stbuf->st_mode = curr_log_entry->inode.mode;
+    stbuf->st_nlink = curr_log_entry->inode.links;
+    stbuf->st_uid = curr_log_entry->inode.uid;
+	stbuf->st_gid = curr_log_entry->inode.gid;
+	stbuf->st_mtime = curr_log_entry->inode.mtime;
+    stbuf->st_size = curr_log_entry->inode.size; //might need to change this later
     return 0;
 }
 
 // File methods
 
 // Create an empty file
-static int wfs_mknod(const char *path, struct fuse_file_info *fi)
+static int wfs_mknod(const char* path, mode_t mode, dev_t rdev)
 {
 
     return 0;
 }
 
 // Create an empty directory
-static int wfs_mkdir(const char *path, mode_t mode, struct fuse_file_info *fi)
+static int wfs_mkdir(const char* path, mode_t mode)
 {
     return 0;
 }
@@ -97,7 +158,7 @@ static int wfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 }
 
 // Remove an existing file
-static int wfs_unlink(const char *path, struct fuse_file_info *fi)
+static int wfs_unlink(const char* path)
 {
     return 0;
 }
@@ -121,7 +182,10 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char *disk_path = argv[argc-1];
+    char *disk_path = argv[argc-2];
+    argv[argc-2] = argv[argc-1];
+    argv[argc -1] = NULL;
+    argc--;    
 
     int fd = open(disk_path, O_CREAT | O_RDWR, 0644);
     if(fd == -1)
@@ -138,15 +202,15 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     //reset the offset to the beginning for the mmap
-    start_of_logs = lseek(fd, sizeof(struct wfs_sb), SEEK_SET);
+    start_of_logs = lseek(fd, sizeof(struct wfs_sb), SEEK_END);
     lseek(fd, 0, SEEK_SET);
-    void *mapped_mem = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    mapped_mem = mmap(NULL, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (mapped_mem == MAP_FAILED) {
         perror("Error mapping file into memory");
         exit(EXIT_FAILURE);
     }
 
-    char *mount_point = argv[argc-2];
+    //char *mount_point = argv[argc-2];
     //note that the call to fuse_main wil have argv stil inclue the disk_path and mount_point
-    return fuse_main(argc-2, argv, &ops, NULL);
+    return fuse_main(argc, argv, &ops, NULL );
 }
